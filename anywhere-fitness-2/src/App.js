1a438d18cdeeb8196c68aa8404100243caa19a66
Justin Peczenij <justinpeczenij@gmail.com>
2022-01-24 19:39:31 -0500

    wip

--- anywhere-fitness-2/src/App.js
+++ anywhere-fitness-2/src/App.js
@@ -1,32 +1,29 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { connect } from 'react-redux';
-import './App.css';
-import ClassForm from './components/ClassForm';
+import { connect } from "react-redux";
+import "./App.css";
+import ClassForm from "./components/ClassForm";
 
-import Homepage from './components/Homepage';
-import LoginForm from './components/LoginForm';
-import SignUpForm from './components/SignUpForm';
-import PrivateRoute from './components/PrivateRoute'
-import EditForm from './components/EditForm'
+import Homepage from "./components/Homepage";
+import LoginForm from "./components/LoginForm";
+import SignUpForm from "./components/SignUpForm";
+import PrivateRoute from "./components/PrivateRoute";
+import EditForm from "./components/EditForm";
 
 function App(props) {
   return (
     <Router>
       <div className="App">
         <Switch>
-          <PrivateRoute path='/Home' component={Homepage} />
-          <PrivateRoute path='/AddClass' component={ClassForm} />
-          <PrivateRoute path='/Edit/:class_id' component={EditForm} />
-          <Route exact path='/' component={LoginForm} />
-          <Route path='/Register' component={SignUpForm} />
-          {/* <Route path='/Home' component={Homepage} />
-          <Route path='/AddClass' component={ClassForm} /> */}
-          {/* <Route path='/EditClass' component={EditClass} /> */}
+          <PrivateRoute path="/home" component={Homepage} />
+          <PrivateRoute path="/addclass" component={ClassForm} />
+          <PrivateRoute path="/edit/:class_id" component={EditForm} />
+          <Route exact path="/" component={LoginForm} />
+          <Route path="/register" component={SignUpForm} />
         </Switch>
       </div>
     </Router>
   );
 }
 
-export default connect(null)(App)
+export default connect(null)(App);
